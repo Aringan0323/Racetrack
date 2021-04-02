3a9721153da439165961a71cb9bfee95211d760f
Harris Ripp <hripp@brandeis.edu>
2021-04-02 22:45:52 +0000

    Added to edge detection and bug fixes in preprocess_img

--- src/edge_detection.py
+++ src/edge_detection.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 
+import rospy, cv2, cv_bridge, numpy
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import cv2import numpy as np
+# import cv2import numpy as np
 
 class EdgeDetection:
 
     def __init__(self):
-        self.imgSub = rospy.Subscriber('/triangle_mask', Image, triangleCB)
+        self.img_sub = rospy.Subscriber('/camera/rgb/imgage_raw', Image, self.img_callback)
 
-    def triangleCB(self, msg):
+    def img_callback(self, msg):
+        img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+        plt.imshow(img)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        plt.imshow(gray, cmap='gray')
+
+        kernel_size = 9
+        grayBlur = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+        lowEnd = 30
+        highEnd = 100
+        edges = cv2.Canny(grayBlur, lowEnd, highEnd)
+
+
+        
     
--- src/preprocess_img.py
+++ src/preprocess_img.py
@@ -4,24 +4,26 @@ import rospy
 from sensor_msgs.msg import Image
 import cv2 as cv
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
 class TriangleMask:
     
     def __init__(self):
-        self.img_sub = rospy.Subscriber('/camera/rgb/imgage_raw', Image, img_callback)
+        self.img_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.img_callback)
         self.img_pub = rospy.Publisher('/triangle_mask', Image, queue_size=1)
         self.bridge = CvBridge()
 
     def img_callback(self, msg):
         img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        w = img.size[0]
-        h = img.size[0]
+        w = img.shape[0]
+        h = img.shape[0]
         apex = [int(w/2), int(h/2)]
         left_vert = [0,h]
         right_vert = [w,h]
-        verts = np.array([apex, left_vert, right_vert], np.int8).reshape((-1, 1, 2))
-        masked_img = cv2.fillPoly(img, [verts], color=(0, 0, 255))
-        self.img_pub.publish(masked_img)
+        verts = np.array([[w,h],  [0,h], [int(w/2), int(h/2)]], np.int32)
+        masked_img = cv.fillPoly(img,pts=[verts], color=(0, 0, 255))
+        cv.imshow("Mask", masked_img)
+        self.img_pub.publish(self.bridge.cv2_to_imgmsg(masked_img))
 
 
 if __name__ == "__main__":
