8f7bd7c3443adadf60c6abba32ba5e799ccc54c7
Adam Ring <adamring@brandeis.edu>
2021-04-03 22:17:30 +0000

    bug fixes

--- src/find_centroid.py
+++ src/find_centroid.py
@@ -20,7 +20,7 @@ class findCentroid:
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             cv2.circle(img, (cx, cy), 20, (0,0,255), -1)
-        self.img_pub.publish(cx)
+            self.img_pub.publish(cx)
         
 if __name__ == "__main__":
     rospy.init_node('centroidXVal')
