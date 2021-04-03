d3c8efc4a2a166e877e6b4b184b33ab890adda40
Harris Ripp <hripp@brandeis.edu>
2021-04-03 20:27:02 +0000

    Edits to edge_detection.py

--- src/edge_detection.py
+++ src/edge_detection.py
@@ -4,7 +4,6 @@ import rospy, cv2, cv_bridge, numpy
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sensor_msgs.msg import Image
-# import cv2import numpy as np
 
 class EdgeDetection:
 
@@ -13,25 +12,19 @@ class EdgeDetection:
         self.img_pub = rospy.Publisher('/canny_mask', Image, queue_size=1)
         self.bridge = cv_bridge.CvBridge()
 
-
     def img_callback(self, msg):
         img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        # cv2.imshow(img)
 
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # plt.imshow(gray, cmap='gray')
 
-        kernel_size = 9
-        grayBlur = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+        kernelSize = 9
+        grayBlur = cv2.GaussianBlur(gray, (kernelSize, kernelSize), 0)
 
         lowEnd = 30
         highEnd = 100
         edges = cv2.Canny(grayBlur, lowEnd, highEnd)
 
         self.img_pub.publish(self.bridge.cv2_to_imgmsg(edges))
-
-
-        # plt.imshow(edges, cmap='Greys_r')
 
 if __name__ == "__main__":
     rospy.init_node('canny_mask')
